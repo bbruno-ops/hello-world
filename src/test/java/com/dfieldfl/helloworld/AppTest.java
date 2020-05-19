ffc9cc9e6f7dffbe95d1a57748b19d007c1dcfa3
Brandon Groves <brandon.groves@gmail.com>
2020-05-19 11:54:49 -0400

    resurfaced 2 update

--- src/test/java/com/dfieldfl/helloworld/AppTest.java
+++ src/test/java/com/dfieldfl/helloworld/AppTest.java
@@ -29,7 +29,7 @@ public class AppTest
       try {
         System.out.format("test iteration %d\n", i);
         Thread.sleep(10000);
-        throw new ClassCastException("resurface");
+        throw new ClassCastException("resurface 2");
       } catch ( Exception e ) {
         // swallowed
       }
